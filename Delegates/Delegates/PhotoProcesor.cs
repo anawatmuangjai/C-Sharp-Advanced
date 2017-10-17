356c6de23aeff387edb72fcc5adab902b801f0b1
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-17 15:42:05 +0700

    Add Delegates example

---
+++ Delegates/Delegates/PhotoProcesor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        // Example 3: use Action<>
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+
+        // Example 2: user delegate keyword
+        //public delegate void PhotoFilterHandler(Photo photo);
+
+        //public void Process(string path, PhotoFilterHandler filterHandler)
+        //{
+        //    var photo = Photo.Load(path);
+
+        //    filterHandler(photo);
+
+        //    photo.Save();
+        //}
+
+        // Example 1
+        //public void Process(string path)
+        //{
+        //    var photo = Photo.Load(path);
+
+        //    var filters = new PhotoFilters();
+        //    filters.ApplyBrightness(photo);
+        //    filters.ApplyContrast(photo);
+        //    filters.Resize(photo);
+
+        //    photo.Save();
+        //}
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var procesor = new PhotoProcesor();
+            //procesor.Process("Test.jpg");
+
+            var procesor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            procesor.Process("Test.jpg", filterHandler);
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply Remove RedEye");
+        }
+    }
+}
